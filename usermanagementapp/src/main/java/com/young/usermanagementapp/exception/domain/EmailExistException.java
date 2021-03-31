afefc3a0f175fb78b54cb29fa7f64849c8534066
Young wook Jeon <ela87@naver.com>
2021-03-31 15:02:03 +0900

    Add custom exception handler

---
+++ usermanagementapp/src/main/java/com/young/usermanagementapp/exception/domain/EmailExistException.java
@@ -0,0 +1,8 @@
+package com.young.usermanagementapp.exception.domain;
+
+public class EmailExistException extends Exception {
+
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
--- usermanagementapp/src/main/java/com/young/usermanagementapp/resource/UserResource.java
+++ usermanagementapp/src/main/java/com/young/usermanagementapp/resource/UserResource.java
@@ -1,13 +1,14 @@
 package com.young.usermanagementapp.resource;
 
 import com.young.usermanagementapp.domain.User;
+import com.young.usermanagementapp.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
+@RequestMapping(path = {"/", "/user"})
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser() {
